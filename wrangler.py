4426879d88ab9cbd8ce305bb709f37dd8192bbd8
Giannis Kesidis <giannis.kesidis@geo.uu.se>
2016-06-09 12:28:39 +0200

    read lines in a row and printed

--- wrangler.py
+++ wrangler.py
@@ -3,31 +3,36 @@
 #This program will have as input a species name from a list
 #The output will be anything requested from a number of dictionaries
 
-
 import re
+import sys #for arguments
+import csv
 
-#area_dict ={}
-#age_dict = {}
-#maker_dict = {}
-#reference_dict = {}
-#total_dict ={}
-#age_dict["C.tenella": Sweden]
-#print age_dict
+#arguments = sys.argv
+#species_data = sys.argv[1] #filename is argument 1
+#with open (species_data, 'ru') as f:
+	
+	#data = list (list(rec) for rec in csv.reader(f, delimiter=','))
+#print(reader)
+#	for row in data:
+#		print row[0]
+#		for data in row:
+#			print data
+
 
 #open a file
-datafile = open ("trace.csv","rw+")
-print "Name of file:", datafile.name
 
-list = open ("trace.csv").readlines()
+datafile = open ("trace.csv","r")
+
+for Line in datafile:
+	Line = Line.strip()
+	print (Line)
+#print "Name of file:", datafile.name
+
 
 #readlines (trace.csv)
-
 #InFileName = trace.csv
-
 #InFile = open ( InFileName, "r" )
-
 #LineNumber = 0
-
 #for Line in InFile:
 #	Line = Line.split(",")
 
@@ -35,7 +40,8 @@ headertypes = []
 
 speciesdata = []
 
-print list
+#print list
 
+datafile.close()
 
  
